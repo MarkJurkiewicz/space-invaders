1dd9478385b8cbd94cb20b5a8d63925763624e6e
Mark Jurkiewicz <markajurkiewicz@gmail.com>
2016-04-13 16:13:39 -0700

    adding the invaders

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
         var screen = canvas.getContext('2d');
         var gameSize = {x: canvas.width, y: canvas.height};
 
-        this.bodies = [new Player(this, gameSize)];
+        this.bodies = createInvaders(this).concat(new Player(this, gameSize));
 
         var self = this;
         var tick = function () {
@@ -63,24 +63,30 @@
         this.game = game;
         this.size = { x: 15, y: 15 };
         this.center = center;
+        this.patrolX = 0;
+        this.speedX = 0.3;
     };
 
-    Player.prototype = {
+    Invader.prototype = {
         update: function () {
-            if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
-                this.center.x -= 2;
-            } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
-                this.center.x += 2;
+            if (this.patrolX < 0 || this.patrolX > 40) {
+            this.speedX = -this.speedX;
             }
 
-            if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
-                var bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.x / 2},
-                    { x: 0, y: -6 });
-                this.game.addBody(bullet);
-
-            }
+            this.center.x += this.speedX;
+            this.patrolX += this.speedX;
         }
     };
+
+    var createInvaders = function(game) {
+        var invaders = [];
+        for (var i = 0; i < 24; i++) {
+            var x = 30 + (i % 8) * 30;
+            var y = 30 + (i % 3) * 30;
+            invaders.push(new Invader(game, { x: x, y: y}));
+        }
+        return invaders;
+    }
 
     var Bullet = function (center, velocity) {
         this.size = { x: 3, y: 3 };
